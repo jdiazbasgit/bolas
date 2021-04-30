59488127b00957ac5cced313f34c1f1bf373ff46
Tu Nombre <you@example.com>
2021-04-30 15:35:38 +0200

    oo

--- src/curso/java/eventos/ElQueSabeLoQueHayQueHacerCopnElRaton.java
+++ src/curso/java/eventos/ElQueSabeLoQueHayQueHacerCopnElRaton.java
@@ -3,51 +3,46 @@ package curso.java.eventos;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Rectangle2D;
+import java.util.Optional;
 
 import curso.java.bolas.Bola;
 import curso.java.hilos.BolaHilo;
 import curso.java.ventanas.VentanaBolas;
 
-public class ElQueSabeLoQueHayQueHacerCopnElRaton implements MouseListener
-{
+public class ElQueSabeLoQueHayQueHacerCopnElRaton implements MouseListener {
 	private VentanaBolas ventanaBolas;
 
-	public ElQueSabeLoQueHayQueHacerCopnElRaton(VentanaBolas ventanaBolas)
-	{
-		this.ventanaBolas=ventanaBolas;
+	public ElQueSabeLoQueHayQueHacerCopnElRaton(VentanaBolas ventanaBolas) {
+		this.ventanaBolas = ventanaBolas;
 	}
-	
-	public int getSentido()
-	{
-		int salida=1;
-		double aleatorio=Math.random();
-		if(aleatorio<0.5)
-			salida=-1;
-		
+
+	public int getSentido() {
+		int salida = 1;
+		double aleatorio = Math.random();
+		if (aleatorio < 0.5)
+			salida = -1;
+
 		return salida;
 	}
-	
-	public int getIncremento()
-	{
-		int salida=1;
-		double aleatorio=Math.random();
-		if(aleatorio<0.5)
-			salida=2;
-		
+
+	public int getIncremento() {
+		int salida = 1;
+		double aleatorio = Math.random();
+		if (aleatorio < 0.5)
+			salida = 2;
+
 		return salida;
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e)
-	{
+	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e)
-	{
-		Bola bola= new Bola();
+	public void mousePressed(MouseEvent e) {
+		Bola bola = new Bola();
 		bola.setPosicionx(e.getX());
 		bola.setPosiciony(e.getY());
 		bola.setIncrementox(getIncremento());
@@ -55,124 +50,87 @@ public class ElQueSabeLoQueHayQueHacerCopnElRaton implements MouseListener
 		bola.setSentidox(getSentido());
 		bola.setSentidoy(getSentido());
 		bola.setDimension(50);
-		
-		/*BolaHilo bolaHilo= new BolaHilo(bola, getVentanaBolas());
-		bolaHilo.start();*/
+
+		/*
+		 * BolaHilo bolaHilo= new BolaHilo(bola, getVentanaBolas()); bolaHilo.start();
+		 */
 		getVentanaBolas().getBolas().add(bola);
-		
-		Runnable bolaHilo=()-> {
-			while(true)
-			{
-				
-				if(bola.getPosicionx()<=0 ||
-						bola.getPosicionx()+
-						bola.getDimension()>=getVentanaBolas().getWidth())
-				{
-					bola.setSentidox(bola.getSentidox()*-1);
+
+		Runnable bolaHilo = () -> {
+			while (true) {
+
+			if (bola.getPosicionx() <= 0
+						|| bola.getPosicionx() + bola.getDimension() >= getVentanaBolas().getWidth()) {
+					bola.setSentidox(bola.getSentidox() * -1);
 				}
-				
-				if(bola.getPosiciony()<=0 ||
-						bola.getPosiciony()+
-						bola.getDimension()>=getVentanaBolas().getHeight())
-				{
-					bola.setSentidoy(bola.getSentidoy()*-1);
+
+				if (bola.getPosiciony() <= 0
+						|| bola.getPosiciony() + bola.getDimension() >= getVentanaBolas().getHeight()) {
+					bola.setSentidoy(bola.getSentidoy() * -1);
 				}
-				
-				
-				try
-				{
-					for (Bola otra : getVentanaBolas().getBolas())
-					{
-						if (!bola.equals(otra))
-						{
-							Rectangle2D miRectangulo = new Rectangle2D.Double(bola.getPosicionx(),
-									bola.getPosiciony(), bola.getDimension(), bola.getDimension());
-							Rectangle2D otroRectangulo = new Rectangle2D.Double(otra.getPosicionx(), otra.getPosiciony(),
-									otra.getDimension(), otra.getDimension());
-							if (miRectangulo.intersects(otroRectangulo))
-							{
-								bola.setSentidox(bola.getSentidox() * -1);
-								otra.setSentidox(otra.getSentidox() * -1);
-								bola.setSentidoy(bola.getSentidoy() * -1);
-								otra.setSentidoy(otra.getSentidoy() * -1);
-								bola.setImpactos(bola.getImpactos()+1);
-								otra.setImpactos(otra.getImpactos()+1);
-							} 
-						}
-						
-					}
-				} catch (Exception e1)
-				{
-					e1.printStackTrace();
-				}
-			
-				
-				
-				bola.setPosicionx(bola.getPosicionx()+
-						bola.getSentidox()*bola.getIncrementox());
-				bola.setPosiciony(bola.getPosiciony()+
-						bola.getSentidoy()*bola.getIncrementoy());
-				
-				
-				
-				try
-				{
+
+				// for (Bola otra : getVentanaBolas().getBolas())
+				getVentanaBolas().getBolas().stream().filter((otra) -> !bola.equals(otra)).filter(otra -> {
+					Rectangle2D miRectangulo = new Rectangle2D.Double(bola.getPosicionx(), bola.getPosiciony(),
+							bola.getDimension(), bola.getDimension());
+					Rectangle2D otroRectangulo = new Rectangle2D.Double(otra.ifPresent((otra1)->otra1.getPosicionx()), 
+							otra.ifPresent((otra1)->getPosiciony(),otra1.getDimension(), otra1.getDimension()));
+							
+					return miRectangulo.intersects(otroRectangulo);
+				}).forEach((otra) -> {
+					Optional<Optional<Bola>> otra1=Optional.of(otra);
+					bola.setSentidox(bola.getSentidox() * -1);
+					otra1.ifPresent((otra2)->otra2.setSentidox(otra2.get().getSentidox() * -1));;
+					bola.setSentidoy(bola.getSentidoy() * -1);
+					otra1.ifPresent((otra2)->otra2.setSentidoy(otra2.getSentidoy() * -1));;
+					bola.setImpactos(bola.getImpactos() + 1);
+					otra1.ifPresent((otra2)->otra2.setImpactos( otra2.getImpactos() + 1));
+				});
+
+				bola.setPosicionx(bola.getPosicionx() + bola.getSentidox() * bola.getIncrementox());
+				bola.setPosiciony(bola.getPosiciony() + bola.getSentidoy() * bola.getIncrementoy());
+
+				try {
 					Thread.sleep(5);
-				} catch (InterruptedException e2)
-				{
+				} catch (InterruptedException e2) {
 					// TODO Auto-generated catch block
 					e2.printStackTrace();
 				}
-				if(bola.getImpactos()>500)
-				{
+				if (bola.getImpactos() > 500) {
 					getVentanaBolas().getBolas().remove(bola);
 					break;
 				}
-					
-				
-				
-				
+
 			}
-			
+
 		};
 		new Thread(bolaHilo).start();
-		
-		
-		
-		
-		
-		
 
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e)
-	{
+	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e)
-	{
+	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e)
-	{
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public VentanaBolas getVentanaBolas()
-	{
+	public VentanaBolas getVentanaBolas() {
 		return ventanaBolas;
 	}
 
-	public void setVentanaBolas(VentanaBolas ventanaBolas)
-	{
+	public void setVentanaBolas(VentanaBolas ventanaBolas) {
 		this.ventanaBolas = ventanaBolas;
 	}
 
--- src/curso/java/ventanas/VentanaBolas.java
+++ src/curso/java/ventanas/VentanaBolas.java
@@ -8,147 +8,117 @@ import java.awt.Image;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.List;
+import java.util.Optional;
 
 import curso.java.bolas.Bola;
 import curso.java.eventos.ElQueSabeLoQueHayQueHacerCopnElRaton;
 
-public class VentanaBolas extends Frame implements WindowListener
-{
-	
+public class VentanaBolas extends Frame implements WindowListener {
+
 	private Image imagen;
 	private Graphics externo;
-	private List<Bola> bolas;
-	
-	public VentanaBolas()
-	{
+	private List<Optional<Bola>> bolas;
+
+	public VentanaBolas() {
 		setSize(500, 500);
-		
+
 		this.addWindowListener(this);
 		this.addMouseListener(new ElQueSabeLoQueHayQueHacerCopnElRaton(this));
 	}
-	
+
 	@Override
-	public void update(Graphics g)
-	{
+	public void update(Graphics g) {
 		paint(g);
-	}
-	
-	@Override
-	public void paint(Graphics g)
-	{
-		if(getExterno()==null)
-		{
-			setImagen(createImage(2000,2000));
-			setExterno(getImagen().getGraphics());
-		}
-		
-		getExterno().clearRect(0, 0, getWidth(), getHeight());
-		Font fuente=new Font("helvetica", Font.BOLD, 20);
-		getExterno().setFont(fuente);
-		for (Bola bola : getBolas())
-		{
-			getExterno().setColor(Color.BLACK);
-		getExterno().fillOval(bola.getPosicionx(), 
-				bola.getPosiciony(), 
-				bola.getDimension(), 
-				bola.getDimension());	
-		getExterno().setColor(Color.WHITE);
-		getExterno().drawString(String.valueOf(bola.getImpactos()),
-				bola.getPosicionx()+(bola.getDimension()/2),
-				bola.getPosiciony()+(bola.getDimension()/2));
-		}
-		
-		
-		
-			g.drawImage(getImagen(), 0, 0, this);
-			
-		
 	}
 
 	@Override
-	public void windowOpened(WindowEvent e)
-	{
+	public void paint(Graphics g) {
+		if (getExterno() == null) {
+			setImagen(createImage(2000, 2000));
+			setExterno(getImagen().getGraphics());
+		}
+
+		getExterno().clearRect(0, 0, getWidth(), getHeight());
+		Font fuente = new Font("helvetica", Font.BOLD, 20);
+		getExterno().setFont(fuente);
+		// for (Bola bola : getBolas())
+		getBolas().forEach((bola) ->{
+			getExterno().setColor(Color.BLACK);
+			bola.ifPresent((bola1)-> getExterno().fillOval(bola1.getPosicionx(), bola1.getPosiciony(), bola1.getDimension(), bola1.getDimension())) ;
+			bola.ifPresent((bola1)->getExterno().setColor(Color.WHITE)); 
+			bola.ifPresent((bola1)->getExterno().drawString(String.valueOf(bola1.getImpactos()), bola1.getPosicionx() + (bola1.getDimension() / 2),
+					bola1.getPosiciony() + (bola1.getDimension() / 2))); 
+		});
+
+		g.drawImage(getImagen(), 0, 0, this);
+
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void windowClosing(WindowEvent e)
-	{
+	public void windowClosing(WindowEvent e) {
 		System.exit(0);
 	}
 
 	@Override
-	public void windowClosed(WindowEvent e)
-	{
+	public void windowClosed(WindowEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void windowIconified(WindowEvent e)
-	{
+	public void windowIconified(WindowEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void windowDeiconified(WindowEvent e)
-	{
+	public void windowDeiconified(WindowEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void windowActivated(WindowEvent e)
-	{
+	public void windowActivated(WindowEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void windowDeactivated(WindowEvent e)
-	{
+	public void windowDeactivated(WindowEvent e) {
 		// TODO Auto-generated method stub
 
 	}
 
-
-
-	public Image getImagen()
-	{
+	public Image getImagen() {
 		return imagen;
 	}
 
-
-
-	public void setImagen(Image imagen)
-	{
+	public void setImagen(Image imagen) {
 		this.imagen = imagen;
 	}
 
-
-
-	public Graphics getExterno()
-	{
+	public Graphics getExterno() {
 		return externo;
 	}
 
-
-
-	public void setExterno(Graphics externo)
-	{
+	public void setExterno(Graphics externo) {
 		this.externo = externo;
 	}
 
-	public List<Bola> getBolas()
-	{
+	public List<Optional<Bola>> getBolas() {
 		return bolas;
 	}
 
-	public void setBolas(List<Bola> bolas)
-	{
+	public void setBolas(List<Optional<Bola>> bolas) {
 		this.bolas = bolas;
 	}
+
+	
 
 }
